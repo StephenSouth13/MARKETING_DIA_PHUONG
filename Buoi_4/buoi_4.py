9039324c219d6202fecfd61c4567a88e39293cc3
Quách Thành Long <stephensouth1307@gmail.com>
2026-01-23 19:06:20 +0700

    Update Buổi 4

---
+++ Buoi_4/buoi_4.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+# Load dữ liệu
+df = pd.read_csv("retail_cluster_simulation.csv")
+
+# Tổng doanh thu theo mô hình
+summary = df.groupby("Model")["Revenue_million"].sum()
+print(summary)
+
+# So sánh trung bình mỗi cửa hàng
+avg_store = df.groupby(["Model","Store"])["Revenue_million"].mean()
+print(avg_store)
+
+import matplotlib.pyplot as plt
+
+summary.plot(kind="bar")
+plt.title("So sánh doanh thu: Cụm KHÁC hãng vs CÙNG hãng")
+plt.ylabel("Doanh thu (triệu VND)")
+plt.show()
